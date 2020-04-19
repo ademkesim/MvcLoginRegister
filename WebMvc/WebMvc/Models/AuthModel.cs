3b85d7587e9c9315ca7845f9c61812ec71d2ef13
kaktimurf <57290668+kaktimurf@users.noreply.github.com>
2020-04-19 15:02:37 +0300

    AuthModel eklendi

---
+++ WebMvc/WebMvc/Models/AuthModel.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models
+{
+    public class AuthModel
+    {
+        public UserForLoginDto userForLoginDto { get; set; }
+        public UserForRegisterDto userForRegisterDto { get; set; }
+    }
+}
